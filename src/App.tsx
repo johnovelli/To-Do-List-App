205a312f365ebcfc3865c74c34e3c5d5178ac926
joh-novelli <joh.novelli90@gmail.com>
2023-12-21 16:33:58 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import Main from './pages/Main';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <Main />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.xvxcv
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
