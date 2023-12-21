394ed12ccf93568caa6b09208fb58157dfbcb113
joh-novelli <joh.novelli90@gmail.com>
2023-12-21 19:30:16 -0300

    Arrumando versao mobile

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import './text-input.css'
 
-function TextInput() {
+type TextInputType = {
+  language: "Pt_br" | "Eng_us",
+  checkedList: string[];
+  setTodoList: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+function TextInput({ language, setTodoList }: TextInputType) {
+  const [inputText, setInputText] = useState<string>("");
+  
+  function handleAdd() {
+    if (inputText === "") {
+      window.alert("Digite uma tarefa");
+      return false;
+    }
+    setTodoList((prev) => [...prev, inputText]);
+    setInputText("");
+  }
+
   return (
     <div className='text-input'>
-    <input type='text' />
+      <input 
+      onChange={(e) => setInputText(e.target.value)}
+      type='text'
+      placeholder={language === 'Pt_br' ? 'Digite uma tarefa' : 'Type a task'}
+      value={inputText}
+      />
+      <button onClick={handleAdd}><strong>Add</strong></button>
     </div>
   );
 }
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,41 @@
+import { useState, useEffect } from 'react';
 import TextInput from '../../components/TextInput';
 import './main.css'
 
 function Main() {
+  const [language, setLanguage] = useState<'Pt_br' | 'Eng_us'>('Pt_br');
+  const [todoList, setTodoList] = useState<string[]>([]);
+  const [checkedList, setCheckedList] = useState<string[]>([]);
+
+  function togglePtBr() {
+    setLanguage('Pt_br')
+  }
+
+  function toggleEngUs() {
+    setLanguage('Eng_us')
+  }
+
+  useEffect(() => {
+    console.log(todoList);
+  }, [todoList])
+
   return (
     <div className='main'>
       <div className='to-do-list'>
-        <h2>Quais os planos para hoje ?</h2>
-        <TextInput />
+        <div className='language'>
+          <button onClick={togglePtBr}>Pt</button>
+          <button onClick={toggleEngUs}>En</button>
+        </div>
+        <h2>{language === 'Pt_br' ? (
+          'Quais os planos para hoje ?'
+        ) : (
+          'What are the plans for today?'
+        )}</h2>
+        <TextInput
+        language={language} 
+        checkedList={checkedList}
+        setTodoList={setTodoList}
+        />
       </div>
     </div>
   );
